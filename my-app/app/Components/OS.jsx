1a94be02327652a66fa5034aec9f3441f66acd19
Vic_ITug <vicitug@gmail.com>
2023-10-02 00:47:40 -0700

    first commit

---
+++ my-app/app/Components/OS.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const OS = () =>{
+    return(
+        <div className="Card">
+            <h3 className="card-title">
+                Title
+            </h3>
+            <div className="card-body">
+                <p className="card-content">
+                    This is the card content
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default OS
---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+import OS, { Service } from "./Components/OS";
+import { Package } from "./Components/package";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>OPERATING SYSTEM FUNCTIONS</h1>
+      <OS/>
+    </main>
+  )
+}
